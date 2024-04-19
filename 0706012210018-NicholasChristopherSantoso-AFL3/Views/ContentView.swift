951f674e82857c951a95005429b0589e81234b8d
Nicholascs <nicholaschristopher565@gmail.com>
2024-04-19 10:47:31 +0700

    section 2 don

--- 0706012210018-NicholasChristopherSantoso-AFL3/Views/ContentView.swift
+++ 0706012210018-NicholasChristopherSantoso-AFL3/Views/ContentView.swift
@@ -13,40 +13,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Yumi The Shih Tzu")
-                    .font(.title)
-                    .foregroundColor (.green)
-                HStack {
-                    Text("I Eat Poop")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("Good Girl")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                
-                
-                Text("About Yumi")
-                    .font(.title2)
-                Text("Sleep, Eat, Repeat")
-                
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
