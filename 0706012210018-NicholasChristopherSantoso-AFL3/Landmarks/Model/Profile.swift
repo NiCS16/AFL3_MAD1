9bf39644024d836ca8e0a1912ad817346e464c07
Nicholascs <nicholaschristopher565@gmail.com>
2024-05-03 10:10:22 +0700

    watchos

---
+++ 0706012210018-NicholasChristopherSantoso-AFL3/Landmarks/Model/Profile.swift
@@ -0,0 +1,30 @@
+//
+//  Profile.swift
+//  0706012210018-NicholasChristopherSantoso-AFL3
+//
+//  Created by student on 02/05/24.
+//
+
+import Foundation
+
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by student on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
